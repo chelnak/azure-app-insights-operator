a569638bf58165cb54e39f663e6f72a6fe114cd4
Craig Gumbley <craiggumbley@gmail.com>
2020-01-28 16:25:56 +0000

    Add ability to pas spec to ai handlers

--- operator/app_insights_operator.py
+++ operator/app_insights_operator.py
@@ -1,62 +1,128 @@
 import kopf
 from app_insights import app_insights
-from utilities import kubernetes
+from kube_handlers import kubernetes
 from config import config as c
 
 
 @kopf.on.create(c.GROUP, c.VERSION, c.PLURAL)
 def create(meta, spec, namespace, logger, **kwargs):
 
-    name = meta.get('name')
+    try:
+        s = {}
+        for k, v in spec.items():
+            s[k] = v
 
-    resource_group = spec.get('resourcegroup')
-    if not resource_group:
-        raise kopf.PermanentError(
-            f"Property resourcegroup must be set. Got {resource_group!r}."
+        name = meta.get('name')
+        resource_group = s.pop('resourcegroup')
+        location = s.pop('location')
+
+        labels = meta.get('labels', {})
+        tags = {'k8s_namespace': namespace}
+        for k, v in labels.items():
+            tags[f"k8s_{k}"] = v
+
+        logger.info(f"Creating app insights resource: {name}.")
+        resourceid, instrumentation_key = app_insights.create_or_update(
+            resource_name=name,
+            resource_group_name=resource_group,
+            location=location,
+            tags=tags,
+            spec=s
         )
 
-    location = spec.get('location')
-    if not location:
-        raise kopf.PermanentError(
-            f"Property location must be set. Got {location!r}."
+        logger.info("Creating child secret with instrumentation key.")
+        kubernetes.create_namespaced_secret(
+            instrumentation_key, name, namespace)
+
+        logger.info(
+            f"Application Insights {name} created."
         )
 
-    logger.info(f"Creating app insights resource: {name}.")
-    resourceid, instrumentation_key = app_insights.create_or_update(
-        resource_group,
-        name,
-        location,
-        {'namespace': namespace}
-    )
+        return {
+            'name': name,
+            'resourceid': resourceid,
+            'instrumentationkey': instrumentation_key
+        }
+    except RuntimeError as e:
+        raise kopf.PermanentError(
+            f"Failed to create app insights resource {name}: {e}")
 
-    logger.info("Creating child secret with instrumentation key.")
-    kubernetes.create_namespaced_secret(instrumentation_key, name, namespace)
 
-    logger.info(
-        f"Application Insights {name} created."
-    )
+@kopf.on.update(c.GROUP, c.VERSION, c.PLURAL)
+def update(name, spec, status, meta, namespace, logger, **kwargs):
 
-    return {
-        'resourceid': resourceid,
-        'instrumentationkey': instrumentation_key
-    }
+    try:
+        logger.info(f"status {status}")
+        logger.info(f"meta {meta}")
+
+        s = {}
+        for k, v in spec.items():
+            s[k] = v
+
+        resource_group = s.pop('resourcegroup')
+        location = s.pop('location')
+
+        labels = meta.get('labels', {})
+        tags = {'k8s_namespace': namespace}
+        for k, v in labels.items():
+            tags[f"k8s_{k}"] = v
+
+        logger.info(f"Updating app insights resource: {name}.")
+        resourceid, instrumentation_key = app_insights.create_or_update(
+            resource_name=name,
+            resource_group_name=resource_group,
+            location=location,
+            tags=tags,
+            spec=s
+        )
+
+    except RuntimeError as e:
+        raise kopf.PermanentError(
+            f"Failed to update app insights resource {name}: {e}")
+
+
+@kopf.on.field(c.GROUP, c.VERSION, c.PLURAL, field='metadata.labels')
+def relabel(spec, status, namespace, old, new, logger, **kwargs):
+
+    try:
+        s = {}
+        for k, v in spec.items():
+            s[k] = v
+
+        name = status.get('create').get('name')
+        resource_group = s.pop('resourcegroup')
+        location = s.pop('location')
+        patch = {'metadata': {'labels': new}}
+
+        kubernetes.patch_namespaced_secret(name, namespace, patch)
+
+        tags = {}
+        for k, v in new.items():
+            tags[f"k8s_{k}"] = v
+
+        logger.info(f"Updating app insights resource tags: {tags}.")
+        resourceid, instrumentation_key = app_insights.create_or_update(
+            resource_name=name,
+            resource_group_name=resource_group,
+            location=location,
+            tags=tags,
+            spec=s
+        )
+
+    except RuntimeError as e:
+        raise kopf.PermanentError(
+            f"Failed to relabel app insights resource {name}: {e}")
 
 
 @kopf.on.delete(c.GROUP, c.VERSION, c.PLURAL)
 def delete(meta, spec, logger, **kwargs):
 
-    name = meta.get('name')
+    try:
+        name = meta.get('name')
+        resource_group = spec.get('resourcegroup')
 
-    resource_group = spec.get('resourcegroup')
-    if not resource_group:
+        app_insights.delete(resource_group, name)
+
+    except RuntimeError as e:
         raise kopf.PermanentError(
-            f"Property resourcegroup must be set. Got {resource_group!r}."
-        )
-
-    location = spec.get('location')
-    if not location:
-        raise kopf.PermanentError(
-            f"Property location must be set. Got {location!r}."
-        )
-
-    app_insights.delete(resource_group, name)
+            f"Failed to delete app insights resource {name}: {e}")
